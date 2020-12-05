7e7f255a6b761a54669ffa013eddbec262a950ff
Christina Owens <owens.christina@gmail.com>
2020-12-04 19:00:06 -0800

    begin playing with styles, clear boilerplate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
@@ -15,41 +13,30 @@ function App() {
     getTime();
   }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <BrowserRouter>
-          <div>
-            <Link className="App-link" to="/">
-              Home
-            </Link>{" "}
-            |{" "}
-            <Link className="App-link" to="/page2">
-              Page 2
-            </Link>
-          </div>
-          <Switch>
-            <Route exact path="/">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-              <p>The current time is {currentTime}.</p>
-            </Route>
-            <Route path="/page2">
-              <p>This is a page 2!</p>
-            </Route>
-          </Switch>
-        </BrowserRouter>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="grid-container">
+          <header>
+            <h1>Stats I Care About</h1>
+            <div>
+              <Link to="/">Home</Link> | <Link to="/page2">Page 2</Link>
+            </div>
+          </header>
+          <main>
+            <Switch>
+              <Route exact path="/">
+                <p>Edit src/App.js and save to reload.</p>
+                <p>The current time is {currentTime}.</p>
+              </Route>
+              <Route path="/page2">
+                <p>This is a page 2!</p>
+              </Route>
+            </Switch>
+          </main>
+          <footer>All Rights Reserved.</footer>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
