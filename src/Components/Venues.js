00e51db18903067bf9a0c1ec368421bb14a682d3
Christina Owens <owens.christina@gmail.com>
2020-12-10 15:14:22 -0800

    Venues.js:  - Add section tags with venue-list and form-cluster classes index.css:  - style list of venues index.html:  - Change title from 'Stats' to 'OpusAdmin'

--- src/Components/Venues.js
+++ src/Components/Venues.js
@@ -3,28 +3,34 @@ import React, { useState, useEffect } from "react";
 export default function Venues() {
   const [venues, setVenues] = useState([]);
 
+  async function getVenues() {
+    const res = await fetch("/api/venues");
+    const data = await res.json();
+    // console.log(data);
+    setVenues(data);
+    // console.log(res);
+  }
   useEffect(() => {
-    async function getVenues() {
-      const res = await fetch("/api/venues");
-      const data = await res.json();
-      // console.log(data);
-      setVenues(data);
-      // console.log(res);
-    }
     getVenues();
+    return () => {
+      setVenues([]);
+    };
   }, []);
 
   return (
     <div>
       <h1 className="row central title">Venues</h1>
-      {venues.map((venue) => (
-        <section key={venue.id} className="card venue">
-          <a href={venue.link} target="_blank" rel="noreferrer">
-            {venue.name}
-          </a>{" "}
-          {venue.address}
-        </section>
-      ))}
+      <section className="form-cluster">Forms will go here.</section>
+      <section className="venue-list">
+        {venues.map((venue) => (
+          <div key={venue.id} className="card venue">
+            <a href={venue.link} target="_blank" rel="noreferrer">
+              {venue.name}
+            </a>{" "}
+            {venue.address}
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
