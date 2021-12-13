817becc2f4e32d1db2600727612ac7d34758c487
Christina Owens <owens.christina@gmail.com>
2021-12-12 20:40:46 -0800

    Revert "Create NoMatch.js as a 404 (default) page"
    This reverts commit f0b8daec4936286b4fbf9c46391f3ff6ee8f1412.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Venues from "./Components/Venues";
 import { useAuth } from "./auth";
 import Finalize from "./Components/Finalize";
 import Invite from "./Components/Invite";
-import NoMatch from "./Components/NoMatch";
 
 function App() {
   const [logged] = useAuth();
@@ -60,16 +59,13 @@ function App() {
                 </Route>
                 <PrivateRoute path="/invite" component={Invite} />
                 <Route>
-                  <Finalize path="finalize" />
+                  <Finalize />
                 </Route>
                 {/* <PrivateRoute path="/finalize" component={Finalize} /> */}
 
                 {/* <Route path="/time">
                   <Time />
                 </Route> */}
-                <Route>
-                  <NoMatch />
-                </Route>
               </Switch>
             </div>
           </main>
